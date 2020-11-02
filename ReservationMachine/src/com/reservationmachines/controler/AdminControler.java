ca36fb2d0da5e0411d317f31ed29453ef0f007e0
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-11-02 22:44:47 +0100

    Ajout des éléments du Controler

---
+++ ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
@@ -0,0 +1,11 @@
+package com.reservationmachines.controler;
+
+import com.reservationmachines.model.AbstractModel;
+
+public class AdminControler extends Controler {
+
+	public AdminControler(AbstractModel model) {
+		super(model);
+	}
+
+}
