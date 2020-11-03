4970f612288b919c5ab063ee1a814c04fffefdb1
megakusa <52033954+megakusa@users.noreply.github.com>
2020-11-03 14:53:29 +0100

    Update AjouterMachineSalleView.java

--- ReservationMachine/src/com/reservationmachines/view/admin/AjouterMachineSalleView.java
+++ ReservationMachine/src/com/reservationmachines/view/admin/AjouterMachineSalleView.java
@@ -1,5 +1,42 @@
 package com.reservationmachines.view.admin;
 
-public class AjouterMachineSalleView {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AjouterMachineSalleView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AjouterMachineSalleView frame = new AjouterMachineSalleView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AjouterMachineSalleView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 
 }
