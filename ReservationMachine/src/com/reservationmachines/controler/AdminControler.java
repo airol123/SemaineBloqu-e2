2caac2d9e0d2e7ff643f555310b6b1f66fb20ead
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-11-03 12:35:58 +0100

    Update AdminControler.java

--- ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
+++ ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
@@ -1,11 +1,20 @@
 package com.reservationmachines.controler;
 
-import com.reservationmachines.model.AbstractModel;
+import com.reservationmachines.model.Etudiant;
+import com.reservationmachines.model.Model;
 
 public class AdminControler extends Controler {
 
-	public AdminControler(AbstractModel model) {
+	private Model model;
+	
+	public AdminControler(Model model) {
 		super(model);
+	}
+
+	public void creerCompteEtudiant(
+			String noEtudiant, String mdp, String email, String nom, String prenom) {
+		Etudiant etudiant = new Etudiant(noEtudiant, mdp, email, nom, prenom);
+		model.creerCompteEtudiant(etudiant);
 	}
 
 }
