23a6ac3fbedddaf577c7946ff3ab24d5e5754fc6
airol123 <carolinerrh0322@gmail.com>
2020-11-03 18:53:43 +0100

    ajouter constructeur

--- ReservationMachine/src/com/reservationmachines/model/ResponsableTP.java
+++ ReservationMachine/src/com/reservationmachines/model/ResponsableTP.java
@@ -1,11 +1,14 @@
 package com.reservationmachines.model;
 
 public class ResponsableTP extends Utilisateur {
+    public ResponsableTP() {
+    }
+
     public ResponsableTP(String identifiant, String mdp) {
         super(identifiant, mdp);
     }
 
-    public ResponsableTP(String identifiant, String mdp, String nom, String prenom) {
-        super(identifiant, mdp, nom, prenom);
+    public ResponsableTP(String identifiant, String mdp, String nom, String prenom, String email) {
+        super(identifiant, mdp, nom, prenom, email);
     }
 }
