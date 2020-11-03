5e7dfa24337ed668c96946c25ce4947797daffbb
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-11-03 12:36:00 +0100

    Update Model.java

--- ReservationMachine/src/com/reservationmachines/model/Model.java
+++ ReservationMachine/src/com/reservationmachines/model/Model.java
@@ -2,4 +2,9 @@ package com.reservationmachines.model;
 
 public class Model extends AbstractModel {
 
+	// Créer un étudiant dans la BD
+	public void creerCompteEtudiant(Etudiant etudiant) {
+		
+	}
+
 }
