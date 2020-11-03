ed06cded4ff608b08667ea20cccb350c8b6b33ad
megakusa <52033954+megakusa@users.noreply.github.com>
2020-11-03 16:00:06 +0100

    Update Model.java

--- ReservationMachine/src/com/reservationmachines/model/Model.java
+++ ReservationMachine/src/com/reservationmachines/model/Model.java
@@ -1,5 +1,9 @@
 package com.reservationmachines.model;
 
 public class Model extends AbstractModel {
-
+	
+	// ajouter une nouvelle machine et l’affecter à une salle
+	public void setMachineSalle(String nomMachine, String nomSalle){
+		
+	};
 }
