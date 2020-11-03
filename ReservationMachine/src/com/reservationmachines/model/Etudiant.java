7f1e483437e7938bb3039f84573857432cdd6704
airol123 <carolinerrh0322@gmail.com>
2020-11-03 15:58:06 +0100

    ajouter email pour Etudiant

--- ReservationMachine/src/com/reservationmachines/model/Etudiant.java
+++ ReservationMachine/src/com/reservationmachines/model/Etudiant.java
@@ -1,6 +1,7 @@
 package com.reservationmachines.model;
 
 public class Etudiant extends Utilisateur {
+    private String email;
     public Etudiant() {
         super();
     }
@@ -9,7 +10,8 @@ public class Etudiant extends Utilisateur {
         super(identifiant, mdp);
     }
 
-    public Etudiant(String identifiant, String mdp, String nom, String prenom) {
+    public Etudiant(String identifiant, String mdp, String nom, String prenom, String email) {
         super(identifiant, mdp, nom, prenom);
+        this.email = email;
     }
 }
