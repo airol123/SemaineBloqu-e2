f3907836a784a717063127779aec1332f9f55b39
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-11-03 17:25:16 +0100

    Update ReservationMachine.java

--- ReservationMachine/src/com/reservationmachines/model/ReservationMachine.java
+++ ReservationMachine/src/com/reservationmachines/model/ReservationMachine.java
@@ -1,5 +1,36 @@
 package com.reservationmachines.model;
 
+import java.sql.Timestamp;
+
 public class ReservationMachine extends Reservation {
 
+	private Etudiant etudiant;
+	private Machine machine;
+	private Timestamp heureDebut;
+	private Timestamp heureFin;
+	
+	public ReservationMachine(Etudiant etudiant, Machine machine,
+		Timestamp heureDebut, Timestamp heureFin) {
+		this.etudiant = etudiant;
+		this.machine = machine;
+		this.heureDebut = heureDebut;
+		this.heureFin = heureFin;
+	}
+	
+	// Revoie le nom de la machine qui correspond à la réservation
+	public String getNomMachine() {
+		return this.machine.getNomMachine();
+	}
+	// Revoie l'état de la machine courante
+	public String getEtatMachine() {
+		return this.machine.getEtatMachine();
+	}
+	// Renvoie le nom de l'étudiant concerné par la réservation
+	public String getNomEtudiant() {
+		return this.etudiant.getNom();
+	}
+	// Renvoie le prénom de l'étudiant concerné par la réservation
+	public String getPrenomEtudiant() {
+		return this.etudiant.getPrenom();
+	}
 }
