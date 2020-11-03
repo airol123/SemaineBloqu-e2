b9dd42fd54c036b0ba574d538190cd3e82b97751
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-11-03 17:25:07 +0100

    Update Etudiant.java

--- ReservationMachine/src/com/reservationmachines/model/Etudiant.java
+++ ReservationMachine/src/com/reservationmachines/model/Etudiant.java
@@ -2,4 +2,20 @@ package com.reservationmachines.model;
 
 public class Etudiant extends Utilisateur {
 
+	public Etudiant(String numEtudiant, String mdp, String email, String nom, String prenom) {
+		this.identifiant = numEtudiant;
+		this.mdp = mdp;
+		this.email = email;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public String getPrenom() {
+		return this.prenom;
+	}
+
 }
