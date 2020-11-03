a7549287b5d20917efff7ce9696538f66a647889
megakusa <52033954+megakusa@users.noreply.github.com>
2020-11-03 16:00:11 +0100

    Update AdminControler.java

--- ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
+++ ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
@@ -1,11 +1,18 @@
 package com.reservationmachines.controler;
 
-import com.reservationmachines.model.AbstractModel;
+import com.reservationmachines.model.Model;
+import com.reservationmachines.model.Machine;
 
 public class AdminControler extends Controler {
 
-	public AdminControler(AbstractModel model) {
+	public AdminControler(Model model) {
 		super(model);
+	}
+
+	public void ajouterMachineSalle(String nomMachine, String nomSalle) {
+		Machine machine = new Machine(nomMachine);
+		model.setMachineSalle(nomMachine, nomSalle);
+		
 	}
 
 }
