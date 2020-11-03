7bd962c676ef4b324cf3ef7c6eaf52faa82ba0ff
megakusa <52033954+megakusa@users.noreply.github.com>
2020-11-03 20:29:45 +0100

    Update Model.java

--- ReservationMachine/src/com/reservationmachines/model/Model.java
+++ ReservationMachine/src/com/reservationmachines/model/Model.java
@@ -1,9 +1,8 @@
 package com.reservationmachines.model;
 
 public class Model extends AbstractModel {
-
-	// Créer un étudiant dans la BD
-	public void creerCompteEtudiant(Etudiant etudiant) {
+	
+	public void creerCompteResponsableTP(ResponsableTP responsableTP) {
 		
 	}
 
