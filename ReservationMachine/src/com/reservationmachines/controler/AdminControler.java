8ee108ebaa464ac6de251030ca6add458fa12941
megakusa <52033954+megakusa@users.noreply.github.com>
2020-11-03 22:37:12 +0100

    Update AdminControler.java

--- ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
+++ ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
@@ -1,6 +1,7 @@
 package com.reservationmachines.controler;
 
 import com.reservationmachines.model.AbstractModel;
+import com.reservationmachines.model.Admin;
 import com.reservationmachines.model.Etudiant;
 import com.reservationmachines.model.Model;
 
@@ -9,7 +10,7 @@ public class AdminControler extends Controler {
 
 	private AbstractModel model;
 	
-	public AdminControler(Model model) {
+	public AdminControler(AbstractModel model) {
 		super(model);
 	}
 
@@ -20,6 +21,12 @@ public class AdminControler extends Controler {
 			same = true;
 		}*/  // la methode pour verifier le mdp
 		return same;
+	}
+
+
+	public void creerCompteResponsableTP(String text, String text2, String text3, String text4, String text5) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
