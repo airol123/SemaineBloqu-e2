de96e0f2eb9b188c2b4f034b8b1fce9f6b6811c6
megakusa <52033954+megakusa@users.noreply.github.com>
2020-11-03 20:29:51 +0100

    Update AdminControler.java

--- ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
+++ ReservationMachine/src/com/reservationmachines/controler/AdminControler.java
@@ -1,20 +1,21 @@
 package com.reservationmachines.controler;
 
+import com.reservationmachines.model.AbstractModel;
 import com.reservationmachines.model.Etudiant;
 import com.reservationmachines.model.Model;
 
 public class AdminControler extends Controler {
 
-	private Model model;
+	private AbstractModel model;
 	
 	public AdminControler(Model model) {
 		super(model);
 	}
 
-	public void creerCompteEtudiant(
-		String noEtudiant, String mdp, String email, String nom, String prenom) {
-		Etudiant etudiant = new Etudiant(noEtudiant, mdp, email, nom, prenom);
-		model.creerCompteEtudiant(etudiant);
+	public void creerCompteResponsableTP(String id, String mdp, String email, String nom, String prenom) {
+		ResponsableTP responsableTP = new ResponsableTP(id, mdp, email, nom, prenom);
+		model.creerCompteResponsableTP(responsableTP);
+		
 	}
 
 }
