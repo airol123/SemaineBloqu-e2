bd6b0f94bff8189965f2da8084dfa70ecfff1cde
airol123 <carolinerrh0322@gmail.com>
2020-11-04 11:19:58 +0100

    ajouter getter setter

--- ReservationMachine/src/com/reservationmachines/model/Salle.java
+++ ReservationMachine/src/com/reservationmachines/model/Salle.java
@@ -8,13 +8,56 @@ public class Salle {
 	private EtatSalle etatSalle;
 	private ArrayList<Machine> machines;
 
+	public Salle() {
+	}
+
+	public Salle(String nomSalle, int capacite, EtatSalle etatSalle) {
+		this.nomSalle = nomSalle;
+		this.capacite = capacite;
+		this.etatSalle = etatSalle;
+	}
+
 	private void sysout() {
 	// TODO Auto-generated method stub
 
 	}
-	
-	
-	
-	
-	
+
+	public Salle(String nomSalle, int capacite, EtatSalle etatSalle, ArrayList<Machine> machines) {
+		this.nomSalle = nomSalle;
+		this.capacite = capacite;
+		this.etatSalle = etatSalle;
+		this.machines = machines;
+	}
+
+	public String getNomSalle() {
+		return nomSalle;
+	}
+
+	public int getCapacite() {
+		return capacite;
+	}
+
+	public EtatSalle getEtatSalle() {
+		return etatSalle;
+	}
+
+	public ArrayList<Machine> getMachines() {
+		return machines;
+	}
+
+	public void setNomSalle(String nomSalle) {
+		this.nomSalle = nomSalle;
+	}
+
+	public void setCapacite(int capacite) {
+		this.capacite = capacite;
+	}
+
+	public void setEtatSalle(EtatSalle etatSalle) {
+		this.etatSalle = etatSalle;
+	}
+
+	public void setMachines(ArrayList<Machine> machines) {
+		this.machines = machines;
+	}
 }
