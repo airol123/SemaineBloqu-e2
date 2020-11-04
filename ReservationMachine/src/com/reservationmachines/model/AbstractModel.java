788207a9a6d3a30ee681ad6a6c5578f445a93e3a
zhousiyuan114 <zhousiyuan0114@gmail.com>
2020-11-04 14:15:36 +0100

    Mise a jour getvaleurreservation(throw exception)

--- ReservationMachine/src/com/reservationmachines/model/AbstractModel.java
+++ ReservationMachine/src/com/reservationmachines/model/AbstractModel.java
@@ -1,10 +1,11 @@
 package com.reservationmachines.model;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public abstract class AbstractModel {
 
 	public abstract String[] getEnteteReservationMachine();
-	public abstract ArrayList<ReservationMachine> getValeursReservationMachine(String idSalle);
+	public abstract ArrayList<ReservationMachine> getValeursReservationMachine(String idSalle) throws SQLException;
 
 }
