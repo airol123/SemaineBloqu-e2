c8914ef0282fa4ad5b94aaa0d2ab8a94c8bc758c
airol123 <carolinerrh0322@gmail.com>
2020-11-04 19:56:22 +0100

    update

--- ReservationMachine/src/com/reservationmachines/view/etudiant/EffectuerReservationMachineView.java
+++ ReservationMachine/src/com/reservationmachines/view/etudiant/EffectuerReservationMachineView.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import com.reservationmachines.controler.EtudiantControler;
 
 import java.awt.*;
+import java.net.URL;
 
 public class EffectuerReservationMachineView {
     private JFrame jFrame=new JFrame("Reserver la machine");
@@ -13,7 +14,22 @@ public class EffectuerReservationMachineView {
     //composition
     private JLabel titre = new JLabel("Reserver la machine");
     private JLabel lbIMsgC = new JLabel();
+    private JLabel lbBon = new JLabel("Bonjour, ");
+    private JLabel lbNom = new JLabel();
+    private JLabel lbSal = new JLabel("Salle");
+    private JLabel lbSalle = new JLabel();
+    private JLabel lbDeb = new JLabel("Debut");
+    private JLabel lbDebur = new JLabel();
+    private JLabel lbF = new JLabel("Fin");
+    private JLabel lbFin = new JLabel();
+    private JButton btnRetour = new JButton("Retourner");
+    private JButton btnValider = new JButton("Valider");
 
+    // background && font
+    private JLabel lblBackground = new JLabel();
+    private URL resource = this.getClass().getResource("images/background2.jpg");
+    private ImageIcon icon = new ImageIcon("images/background2.jpg");
+    private Font font = new Font("Arial", Font.BOLD, 36);
 
 
 
