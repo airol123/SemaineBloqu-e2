3696e51c9a5d0b80eea086c8357eeeddb66c7ce5
megakusa <52033954+megakusa@users.noreply.github.com>
2020-11-04 22:19:02 +0100

    Update ConsulterReclamationView.java

--- ReservationMachine/src/com/reservationmachines/view/admin/ConsulterReclamationView.java
+++ ReservationMachine/src/com/reservationmachines/view/admin/ConsulterReclamationView.java
@@ -1,5 +1,54 @@
 package com.reservationmachines.view.admin;
 
-public class ConsulterReclamationView {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.reservationmachines.controler.AdminControler;
+
+public class ConsulterReclamationView extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	
+	private AdminControler controler;
+
+	/**
+	 * Create the frame.
+	 */
+	public ConsulterReclamationView(AdminControler controler) {
+		this.controler = controler;
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Consultation de la réclamation");
+		lblNewLabel.setBounds(10, 10, 220, 15);
+		contentPane.add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 42, 416, 175);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			controler.getReclamation(),
+			new String[] {
+				"Etudient", "Machine", "Type", "Description"
+			}
+		));
+		scrollPane.setViewportView(table);
+		this.setVisible(true);
+	}
 }
