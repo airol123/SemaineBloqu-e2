d598041b5424d4b81a8b9e6dbb9e39dd0d896a1e
airol123 <carolinerrh0322@gmail.com>
2020-11-04 19:42:25 +0100

    update

--- ReservationMachine/src/com/reservationmachines/view/etudiant/EffectuerReservationMachineView.java
+++ ReservationMachine/src/com/reservationmachines/view/etudiant/EffectuerReservationMachineView.java
@@ -4,8 +4,19 @@ import javax.swing.*;
 
 import com.reservationmachines.controler.EtudiantControler;
 
+import java.awt.*;
+
 public class EffectuerReservationMachineView {
     private JFrame jFrame=new JFrame("Reserver la machine");
+    private EtudiantControler ec;
+    private Container c = jFrame.getContentPane();
+    //composition
+    private JLabel titre = new JLabel("Reserver la machine");
+    private JLabel lbIMsgC = new JLabel();
+
+
+
+
     
     private EtudiantControler controler;
     
