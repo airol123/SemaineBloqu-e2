f5e02bf527c4fea4d3d1d26df902e835b4ac52f9
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-11-04 12:39:20 +0100

    Retrait des PagesPrincipales et Ajout dans le bon package ("view.main")

--- ReservationMachine/src/com/reservationmachines/view/main/ResponsableTPMainView.java
+++ ReservationMachine/src/com/reservationmachines/view/main/ResponsableTPMainView.java
@@ -1,5 +1,11 @@
 package com.reservationmachines.view.main;
 
+import com.reservationmachines.controler.ResponsableTPControler;
+
 public class ResponsableTPMainView {
+
+	public ResponsableTPMainView(ResponsableTPControler controler) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
