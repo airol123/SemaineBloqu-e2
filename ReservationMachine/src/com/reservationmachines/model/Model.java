5f2442adef46a4e1f0d46127130fe69409e433b5
zhousiyuan114 <zhousiyuan0114@gmail.com>
2020-11-04 13:56:16 +0100

    Mise a jour getvaleurreservationmachine

--- ReservationMachine/src/com/reservationmachines/model/Model.java
+++ ReservationMachine/src/com/reservationmachines/model/Model.java
@@ -5,7 +5,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 
 
@@ -17,8 +23,42 @@ public class Model extends AbstractModel {
 	}
 	
 	@Override
-	public ArrayList<ReservationMachine> getValeursReservationMachine(String idSalle) {
-		ArrayList<ReservationMachine> reservations = new ArrayList<ReservationMachine>();
+	public ArrayList<ReservationMachine> getValeursReservationMachine(String idSalle) throws SQLException {
+		ArrayList<ReservationMachine> reservations = new ArrayList<ReservationMachine>();		
+		String sqlreservationm = "select * from salle,machine,reserverm,etudiant where noms=? and salle.IDS=machine.IDS and machine.IDM=reserverm.IDM and reserverm.IDE=etudiant.IDE "; 
+		Connection con =BD.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(sqlreservationm);
+		pstmt.setString(1, idSalle);
+		ResultSet rs=pstmt.executeQuery();
+		while(rs.next()) {
+			Etudiant etu=new Etudiant();
+			etu.setNom(rs.getString("nome"));	
+			etu.setPrenom(rs.getString("prenome"));
+			etu.setEmail(rs.getString("emaile"));
+			etu.setIdentifiant(String.valueOf(rs.getInt("ide")));
+			etu.setMdp(rs.getString("mdpe"));
+			Machine mac=new Machine(rs.getString("nomm"),EtatMachine.valueOf(rs.getString("etatm")));
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");	
+			
+			String sdd=rs.getString("datem")+" "+rs.getString("heuredebutm");
+			Timestamp d = null;
+			try {
+				d = new Timestamp(dateFormat.parse(sdd).getTime());
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+			}
+			
+			String sdf=rs.getString("datem")+" "+rs.getString("heurefinm");
+			Timestamp f = null;
+			try {
+				f = new Timestamp(dateFormat.parse(sdf).getTime());
+			} catch (ParseException e2) {
+				e2.printStackTrace();
+			}
+			
+			reservations.add(new ReservationMachine(etu, mac, d, f));
+		}
+		
 		
 		/*
 		 * Remplir la liste avec toutes les réservations de la salle 'idSalle' en paramètre
@@ -61,7 +101,7 @@ public class Model extends AbstractModel {
 			etu.setMdp(rs.getString("mdpe"));
 		}
 		else {
-			System.out.println("Il y a pas de cet etudiant");
+			System.out.println("Il y a pas de cet etudiant ou en attente");
 		}
 		rs.close();  
 		
@@ -115,6 +155,6 @@ public class Model extends AbstractModel {
 		
 		return restp;
 	}
- 
+
 
 }
