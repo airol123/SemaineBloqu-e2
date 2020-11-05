c761bcf97680ba8c351c5a3785e3300b1c2df2f8
zhousiyuan114 <zhousiyuan0114@gmail.com>
2020-11-05 13:25:02 +0100

    Supprimer reservation etudiant

--- ReservationMachine/src/com/reservationmachines/model/AbstractModel.java
+++ ReservationMachine/src/com/reservationmachines/model/AbstractModel.java
@@ -56,4 +56,5 @@ public abstract class AbstractModel {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	public abstract boolean supprimerRservation(ReservationMachine reservationMachine);
 }
