87330aa496d02429df54de3b4bdbb1ebcbe82d18
airol123 <carolinerrh0322@gmail.com>
2020-11-06 12:03:59 +0100

    update

--- ReservationMachine/src/com/reservationmachines/model/AbstractModel.java
+++ ReservationMachine/src/com/reservationmachines/model/AbstractModel.java
@@ -9,7 +9,6 @@ public abstract class AbstractModel {
 	public abstract ArrayList<ReservationMachine> getValeursReservationMachine(String nomSalle, String date, String heureDebut, String heureFin);
 	public abstract void creerCompteEtudiant(Etudiant etudiant);
 	public abstract void creerCompteResponsableTP(ResponsableTP responsableTP) ;
-	public abstract void setMachineSalle(String nomMachine, String nomSalle);
 	public abstract void ajoutSalle(String nomSalle);
 	public abstract boolean verifierMotDePasseEtudiant(String numEtudiant, String mdp);
 	public abstract boolean verifierMotDePasseResponsableTP(String idResponsableTP, String mdp);
